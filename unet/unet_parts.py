6f936a322a8fbc6fd5243e61192aadf379bda1f8
Huitong Lu <luhuitong2016@gmail.com>
2022-01-11 04:46:27 +0100

    Signed-off-by: Huitong Lu <luhuitong2016@gmail.com>

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -54,7 +54,7 @@ class Up(nn.Module):
             self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
-        x1 = self.up(x1)
+        x1 = self.up(x1) 
         # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
