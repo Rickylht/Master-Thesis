9c5f8bd94aea57be627ca8312e5c019fa0f9de1c
Huitong Lu <luhuitong2016@gmail.com>
2022-01-12 06:49:50 +0100

    Signed-off-by: Huitong Lu <luhuitong2016@gmail.com>

--- train/prepocessing.py
+++ train/prepocessing.py
@@ -1,11 +1,25 @@
 import cv2
 import numpy as np
+import json
 
-def create_mask():
+def create_mask(imgpath, jsonpath):
     '''create single mask for one image and save'''
-    pass
+    with open(jsonpath, "r") as f:
+        data = f.read()
+    
+    jsondata = json.loads(data)
+    for entry in jsondata['shapes']:
+        if entry['label'] == 'tooth':
+            tooth_point_list = entry['points']
+            tooth_point_list = np.array(tooth_point_list, dtype=np.int32)
+            image = cv2.imread(imgpath)
+            mask = np.zeros_like(image, dtype=np.uint8)
+            cv2.fillPoly(mask, [tooth_point_list], (255, 255, 255))
+            cv2.imwrite('.\data\masks\sample.png', mask)
 
-def creat_all_masks():
+#create_mask(".\data\imgs\sample.bmp", ".\data\jsons\sample.json")
+
+def create_all_masks():
     '''create masks for all images and save'''
     pass
 
